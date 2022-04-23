cc29f37d0c089c7ee55257035f9742fb1aadc77a
Yuri Savinykh <yuras2002s@gmail.com>
2022-04-24 00:11:04 +0300

    Goods are done

--- src/main/java/ru/savuri/webprak/web/controller/GoodController.java
+++ src/main/java/ru/savuri/webprak/web/controller/GoodController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.savuri.webprak.model.dao.GoodDAO;
 import ru.savuri.webprak.model.dao.impl.GoodDAOImpl;
@@ -52,5 +53,50 @@ public class GoodController {
         }
 
         return "goodEdit";
+    }
+
+    @PostMapping("/goodSave")
+    public String goodSave(@RequestParam(name = "goodId", required = false) Long goodId,
+                           @RequestParam(name = "assemblyPlace") String assemblyPlace,
+                           @RequestParam(name = "manufacturer") String manufacturer,
+                           @RequestParam(name = "model") String model,
+                           @RequestParam(name = "price") Integer price,
+                           @RequestParam(name = "quantity") Integer quantity,
+                           @RequestParam(name = "typeInput") Good.GoodType typeInput,
+                           @RequestParam(name = "description") String description,
+                           Model prjModel) {
+        if (assemblyPlace == null || manufacturer == null || model == null || price == null
+                || quantity == null || typeInput == null || description == null) {
+            prjModel.addAttribute("errMsg", "One field of form was not field");
+            return "errorPage";
+        }
+
+
+        if (goodId == null) {
+            // create
+            Good good = new Good(model, typeInput, price, quantity, manufacturer, assemblyPlace, description);
+            goodDAO.save(good);
+
+            return String.format("redirect:/goodInfo?goodId=%d", good.getId());
+        } else {
+            // update
+            Good good = goodDAO.getById(goodId);
+            good.setModel(model);
+            good.setType(typeInput);
+            good.setPrice(price);
+            good.setQuantity(quantity);
+            good.setManufacturer(manufacturer);
+            good.setAssemblyPlace(assemblyPlace);
+            good.setDescription(description);
+
+            return String.format("redirect:/goodInfo?goodId=%d", good.getId());
+        }
+    }
+
+    @PostMapping("/goodDelete")
+    public String goodDelete(@RequestParam(name = "goodId") Long goodId) {
+        goodDAO.delete(goodDAO.getById(goodId));
+
+        return "redirect:/goods";
     }
 }
