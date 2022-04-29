fa73f67f511abd536bd82d0be84c08b95fe28aac
Yuri Savinykh <yuras2002s@gmail.com>
2022-04-29 16:09:24 +0300

    Order support

--- src/main/java/ru/savuri/webprak/web/DTO/OrderCreationDTO.java
+++ src/main/java/ru/savuri/webprak/web/DTO/OrderCreationDTO.java
@@ -1,28 +1,28 @@
 package ru.savuri.webprak.web.DTO;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+import ru.savuri.webprak.model.entity.Good;
 
-import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
 
 
-
 /*
- * По науке, как я понимаю, нужно для каждого form dto писать. Но зачем если у меня в других случаях всё прекрасно
+ * По науке, как я понимаю, нужно для каждого формы dto писать. Но зачем если у меня в других случаях всё прекрасно
  * через @RequestParam делалось.
  */
 @Getter
 @Setter
 @NoArgsConstructor
 @RequiredArgsConstructor
-public class OrderDTO {
-    public Long customerId;
-    public Timestamp deliveryTime;
-    public String deliveryPlace;
+public class OrderCreationDTO {
     @NonNull
-    public List<Integer> list;
-
-    public void addToList(Integer someInteger) {
-        list.add(someInteger);
-    }
+    private Long customerId;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    private LocalDateTime deliveryDateTime;
+    @NonNull
+    private String deliveryPlace;
+    @NonNull
+    private List<Integer> purchaseQuantity;
 }
--- src/main/java/ru/savuri/webprak/web/controller/OrderController.java
+++ src/main/java/ru/savuri/webprak/web/controller/OrderController.java
@@ -1,22 +1,116 @@
 package ru.savuri.webprak.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ru.savuri.webprak.model.dao.*;
+import ru.savuri.webprak.model.entity.Good;
+import ru.savuri.webprak.model.entity.Order;
+import ru.savuri.webprak.model.entity.OrderGood;
+import ru.savuri.webprak.model.entity.User;
+import ru.savuri.webprak.web.DTO.OrderCreationDTO;
+
+import java.sql.Time;
+import java.util.*;
 
 @Controller
 public class OrderController {
+    private final OrderDAO orderDAO;
+    private final GoodDAO goodDAO;
+    private final OrderGoodDAO orderGoodDAO;
+
+    private final UserDAO userDAO;
+
+    OrderController(OrderDAO orderDAO, GoodDAO goodDAO, OrderGoodDAO orderGoodDAO, UserDAO userDAO) {
+        this.orderDAO = orderDAO;
+        this.goodDAO = goodDAO;
+        this.orderGoodDAO = orderGoodDAO;
+        this.userDAO = userDAO;
+    }
+
     @GetMapping("/orders")
-    public String orders() {
+    public String orders(Model model) {
+        List<Order> orderList = orderDAO.getAll();
+        model.addAttribute("orderList", orderList);
         return "orders";
     }
 
     @GetMapping("/orderInfo")
-    public String orderInfo() {
+    public String orderInfo(@RequestParam(name = "orderId") Long orderId,
+                            Model model) {
+        Order order = orderDAO.getById(orderId);
+        model.addAttribute("order", order);
+
         return "orderInfo";
     }
 
     @GetMapping("/orderCreate")
-    public String orderCreate() {
+    public String orderCreate(Model model) {
+        List<Good> goodList = goodDAO.getAll();
+        List<Integer> purchaseQuantity = new ArrayList<Integer>(goodList.size());
+
+        for (int i = 0; i < goodList.size(); ++i) purchaseQuantity.add(0);
+
+        OrderCreationDTO orderCreationDTO = new OrderCreationDTO();
+        orderCreationDTO.setPurchaseQuantity(purchaseQuantity);
+
+        model.addAttribute("goodList", goodList);
+        model.addAttribute("form", orderCreationDTO);
+
         return "orderCreate";
+    }
+
+    @PostMapping("orderSave")
+    public String orderSave(@ModelAttribute(name = "form") OrderCreationDTO orderCreationDTO, Model model) {
+        Order order = new Order();
+
+        List<Integer> purchaseQuantity = orderCreationDTO.getPurchaseQuantity();
+        List<Good> goodList = goodDAO.getAll();
+
+        List<Good> goodsToUpdate = new ArrayList<>();
+        List<OrderGood> orderGoodsToLoad = new ArrayList<>();
+
+        for (int i = 0; i < purchaseQuantity.size(); ++i) {
+            if (purchaseQuantity.get(i) > 0) {
+                Good good = goodList.get(i);
+                OrderGood orderGood = new OrderGood(order, goodList.get(i), purchaseQuantity.get(i), goodList.get(i).getPrice());
+
+                goodsToUpdate.add(good);
+                orderGoodsToLoad.add(orderGood);
+
+                Set<OrderGood> thisGoodOrders = good.getOrderGoods();
+                thisGoodOrders.add(orderGood);
+                good.setOrderGoods(thisGoodOrders);
+            }
+        }
+
+        User customer = userDAO.getById(orderCreationDTO.getCustomerId());
+
+        Set<OrderGood> tmp = new HashSet<>(orderGoodsToLoad.size());
+
+        order.setUser(customer);
+        order.setOrderGoods(tmp);
+        order.setDeliveryPlace(orderCreationDTO.getDeliveryPlace());
+        order.setDeliveryTime(orderCreationDTO.getDeliveryDateTime());
+        order.setStatus(Order.Status.PROCESSING);
+
+        orderDAO.save(order);
+
+        for (int i = 0; i < goodList.size(); ++i) {
+            goodDAO.update(goodList.get(i));
+        }
+
+        return String.format("redirect:/orderInfo?orderId=%d", order.getId());
+    }
+
+    @PostMapping("orderDelete")
+    public String orderDelete(@RequestParam(name = "orderId") Long orderId) {
+        orderDAO.delete(orderDAO.getById(orderId));
+
+
+        return "redirect:/orders";
     }
 }
